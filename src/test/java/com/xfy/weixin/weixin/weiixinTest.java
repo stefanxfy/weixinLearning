503d55c93739b4e18cfd1ce73fa0bc1bbbbde612
a <502681863@qq.com>
2017-11-21 23:29:45 +0800

    新增菜单的创建，菜单的查询删除测试方法

--- src/test/java/com/xfy/weixin/weixin/weiixinTest.java
+++ src/test/java/com/xfy/weixin/weixin/weiixinTest.java
@@ -2,6 +2,7 @@ package com.xfy.weixin.weixin;
 
 import com.xfy.weixin.weixin.pojo.AccessToken;
 import com.xfy.weixin.weixin.utils.weixinUtil;
+import net.sf.json.JSONObject;
 
 /**
  * Created by Stefan
@@ -12,6 +13,26 @@ public class weiixinTest {
         AccessToken accessToken= weixinUtil.getAccessToken();
         System.out.println("Access_token:"+accessToken.getAccess_token());
         System.out.println("Expires_in:"+accessToken.getExpires_in());
+        //创建菜单
+        String menu= JSONObject.fromObject(weixinUtil.initMenu()).toString();
+        int result = weixinUtil.createMenu(accessToken.getAccess_token(),menu);
+        System.out.println("创建成功"+result);
+
+        //查询菜单
+        //JSONObject jsonObject=weixinUtil.queryMenu(accessToken.getAccess_token());
+        //System.out.println(jsonObject);
+
+        //删除菜单
+        /*int result = weixinUtil.deleteMenu(accessToken.getAccess_token());
+        if(result==0){
+            System.out.println("菜单删除成功");
+        }else {
+            System.out.println(result);
+        }*/
+
+
+
+
 
     }
 }
