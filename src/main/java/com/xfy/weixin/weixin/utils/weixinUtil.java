a7967c5728e25e7a36d6fff6aabbccffe9ef965f
a <502681863@qq.com>
2017-11-21 23:28:30 +0800

    新增菜单的初始化，菜单的创建，菜单的查询删除方法

--- src/main/java/com/xfy/weixin/weixin/utils/weixinUtil.java
+++ src/main/java/com/xfy/weixin/weixin/utils/weixinUtil.java
@@ -1,5 +1,9 @@
 package com.xfy.weixin.weixin.utils;
 
+import com.xfy.weixin.weixin.menu.Button;
+import com.xfy.weixin.weixin.menu.ClickButton;
+import com.xfy.weixin.weixin.menu.Menu;
+import com.xfy.weixin.weixin.menu.ViewButton;
 import com.xfy.weixin.weixin.pojo.AccessToken;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -20,6 +24,9 @@ public class weixinUtil {
     private static final String APPID="wxcbb7dc0e98cbae1e";
     private static final String APPSECRET="14e79eb419399a6cbfe3aaed36b87eb5";
     private static final String ACCESS_TOKEN_URL="https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET";
+    private static final String CREATE_MENU_URL="https://api.weixin.qq.com/cgi-bin/menu/create?access_token=ACCESS_TOKEN";
+    private static  final String QUERY_MENU_URL="https://api.weixin.qq.com/cgi-bin/menu/get?access_token=ACCESS_TOKEN";
+    private static final String DELETE_MENU_URL="https://api.weixin.qq.com/cgi-bin/menu/delete?access_token=ACCESS_TOKEN";
 /*
 * GET请求
 *
@@ -45,9 +52,9 @@ public class weixinUtil {
 /*
 * post请求
 * */
-    public static JSONObject doPostStr(String outStr)  {
+    public static JSONObject doPostStr(String url,String outStr)  {
         DefaultHttpClient httpClient=new DefaultHttpClient();
-        HttpPost httpPost=new HttpPost();
+        HttpPost httpPost=new HttpPost(url);
         JSONObject jsonObject=null;
         HttpResponse response= null;
         try {
@@ -77,6 +84,70 @@ public class weixinUtil {
         }
         return accessToken;
     }
+/*
+* 菜单组装
+* */
+    public static Menu initMenu(){
+        Menu menu=new Menu();
+        ClickButton clickButton1=new ClickButton();
+        clickButton1.setName("主菜单一");
+        clickButton1.setType("click");
+        clickButton1.setKey("11");
+
+        ViewButton viewButton=new ViewButton();
+        viewButton.setName("主菜单二");
+        viewButton.setType("view");
+        viewButton.setUrl("http://www.baidu.com");
+
+        Button button=new Button();
+        button.setName("主菜单三");
+        ClickButton  clickButton2=new ClickButton();
+        clickButton2.setName("扫码");
+        clickButton2.setType("scancode_push");
+        clickButton2.setKey("31");
+
+        ClickButton  clickButton3=new ClickButton();
+        clickButton3.setName("地理位置");
+        clickButton3.setType("location_select");
+        clickButton3.setKey("32");
+
+        button.setSub_button(new Button[]{clickButton2,clickButton3});
+
+        menu.setButton(new Button[]{clickButton1,viewButton,button});
+        return menu;
+    }
+
+    public static int createMenu(String token,String menu){
+        int result=0;
+        String url=CREATE_MENU_URL.replace("ACCESS_TOKEN",token);
+        JSONObject jsonObject=doPostStr(url,menu);
+        if(jsonObject!=null){
+            result=jsonObject.getInt("errcode");
+        }
+        return result;
+    }
+/*
+* 查询菜单
+* */
+    public static JSONObject queryMenu(String token){
+        String url=QUERY_MENU_URL.replace("ACCESS_TOKEN",token);
+        JSONObject jsonObject=doGetStr(url);
+        return jsonObject;
+    }
+/*
+* 删除菜单
+* */
+    public static int deleteMenu(String token){
+        String url=DELETE_MENU_URL.replace("ACCESS_TOKEN",token);
+        JSONObject jsonObject=doGetStr(url);
+        int result = 0;
+        if (jsonObject!=null){
+            result=jsonObject.getInt("errcode");
+        }
+        return result ;
+    }
+
+
 
 
 
